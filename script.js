c7366e7e1394d3e43a99511a44755e5339b12f98
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-19 17:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+document.getElementById('submit').addEventListner('click',function(e){
+	e.preventDefault();
+	const title = document.getElementById('title').value.trim();
+	const author = document.getElementById('author').value.trim();
+	cont isbn = document.getElementById('isbn').value.trim();
+
+	if(title==='' || author==='' || isbn===''){
+		 alert('please in all fields');
+		return;
+	}
+	const booklist = document.getElementById('book-list');
+	const row  = document.createElement('tr');
+	row.innerHTML = 
+		<td>${title}</td>
+	    <td>${author}</td>
+		<td>${isbn}</td>;
+
+	booklist.appendChild(row);
+document.getElementById('title').value='';
+document.getElementById('author').value='';
+document.getElementById('isbn').value='';
+
+												  
+});
+document.getElementById('book-list').addEventListner('click',function(e){
+	if(e.target.classList.contains('delete')){
+		e.target.closest('tr').remove();
+	}
+	
+});
+
